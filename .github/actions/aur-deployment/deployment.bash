6395a332228f3cf5ec6a0f3b04ae00f0672ccc59
Israel Roldan <israel.alberto.rv@gmail.com>
2023-08-04 20:16:12 -0600

    Updated deployment files.

--- .github/actions/aur-deployment/deployment.bash
+++ .github/actions/aur-deployment/deployment.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set +xve
 
+# Check if the environment variable was set from Docker. Otherwise, exit.
 validate_environment_variable() {
-  if [[ -z "${!1}" ]]; then
+  if [[ -z ${!1} ]]; then
     echo "ERROR: The environment variable ${1}, needs to be set from Docker."
     exit 1
   else
@@ -11,39 +12,96 @@ validate_environment_variable() {
 }
 
 # Environment variables needs to set from Docker.
-validate_environment_variable ENV_PACKAGE_NAME
-validate_environment_variable ENV_GITHUB_TAG_VERSION
 validate_environment_variable ENV_GITHUB_OWNER
 validate_environment_variable ENV_GITHUB_REPOSITORY
-validate_environment_variable ENV_SSH_PUBLIC_KEY
+validate_environment_variable ENV_PACKAGE_ARCHITECTURES
+validate_environment_variable ENV_PACKAGE_DEPENDENCIES
+validate_environment_variable ENV_PACKAGE_DESCRIPTION
+validate_environment_variable ENV_PACKAGE_INFORMATION
+validate_environment_variable ENV_PACKAGE_LICENSES
+validate_environment_variable ENV_PACKAGE_NAME
+validate_environment_variable ENV_PACKAGE_PGPS
+validate_environment_variable ENV_PACKAGE_RELEASE
+validate_environment_variable ENV_PACKAGE_VERSION
 validate_environment_variable ENV_SSH_PRIVATE_KEY
+validate_environment_variable ENV_SSH_PUBLIC_KEY
+
+# Environment variables which are optional to set from Docker.
+echo "ENV_PACKAGE_SOURCES: ${ENV_PACKAGE_SOURCES}"
+echo "ENV_PACKAGE_SOURCE_GITHUB: ${ENV_PACKAGE_SOURCE_GITHUB}"
+echo "ENV_GITHUB_TAG_VERSION_PREFIX: ${ENV_GITHUB_TAG_VERSION_PREFIX}"
+echo "ENV_GITHUB_TAG_VERSION_SUFFIX: ${ENV_GITHUB_TAG_VERSION_SUFFIX}"
 
 # Environment variables set from Dockerfile.
-validate_environment_variable ENV_USER_ID
 validate_environment_variable ENV_USER_GROUP
-validate_environment_variable ENV_USER_PASSWORD
-validate_environment_variable ENV_BUILD_PACKAGE
-validate_environment_variable ENV_PACKAGE_NAME
 validate_environment_variable ENV_USER_HOME
+validate_environment_variable ENV_USER_ID
+
+# Variables auto-generated. The order could affect the operation.
+AUTO_GITHUB_TAG_VERSION="${ENV_GITHUB_TAG_VERSION_PREFIX}${ENV_PACKAGE_VERSION}${ENV_GITHUB_TAG_VERSION_SUFFIX}"
+AUTO_GITHUB_REPOSITORY_UID="${ENV_GITHUB_OWNER}/${ENV_GITHUB_REPOSITORY}"
+AUTO_GITHUB_URL="https://github.com/${AUTO_GITHUB_REPOSITORY_UID}"
+AUTO_PACKAGE_SOURCES_GIT="${ENV_GITHUB_REPOSITORY}-${ENV_PACKAGE_VERSION}.tar.gz::${AUTO_GITHUB_URL}/archive/refs/tags/${AUTO_GITHUB_TAG_VERSION}.tar.gz"
+AUTO_SOURCE_DOWNLOADED="${ENV_GITHUB_REPOSITORY}-${ENV_PACKAGE_VERSION}"
+AUTO_PACKAGE_SOURCES=""
+if [[ -n ${ENV_PACKAGE_SOURCE_GITHUB} ]]; then
+  AUTO_PACKAGE_SOURCES+="'${AUTO_PACKAGE_SOURCES_GIT}'"
+fi
+if [[ -n ${ENV_PACKAGE_SOURCES} ]]; then
+  if [[ -n ${ENV_PACKAGE_SOURCE_GITHUB} ]]; then
+    AUTO_PACKAGE_SOURCES+=" "
+  fi
+  AUTO_PACKAGE_SOURCES+="${ENV_PACKAGE_SOURCES}"
+fi
+
+echo "AUTO_GITHUB_TAG_VERSION: ${AUTO_GITHUB_TAG_VERSION}"
+echo "AUTO_GITHUB_REPOSITORY_UID: ${AUTO_GITHUB_REPOSITORY_UID}"
+echo "AUTO_GITHUB_URL: ${AUTO_GITHUB_URL}"
+echo "AUTO_PACKAGE_SOURCES_GIT: ${AUTO_PACKAGE_SOURCES_GIT}"
+echo "AUTO_PACKAGE_SOURCES: ${AUTO_PACKAGE_SOURCES}"
+echo "AUTO_SOURCE_DOWNLOADED: ${AUTO_SOURCE_DOWNLOADED}"
+
+# TODO: Figure out a solution for send multiple values in the Environment variables.
+# ENV_PACKAGE_ARCHITECTURES
+# - x86_64
+# - ...
+# ENV_PACKAGE_LICENSES
+# - GPL3
+# - ...
+# ENV_PACKAGE_DEPENDENCIES
+# - bash
+# - xorg-xrandr
+# - grep
+# - coreutils
+# - sed
+# - bc
+# AUTO_PACKAGE_SOURCES
+# - 'ENV_GITHUB_REPOSITORY-ENV_PACKAGE_VERSION.tar.gz::AUTO_GITHUB_URL/archive/refs/tags/ENV_GITHUB_TAG_VERSION_PREFIX.tar.gz'
+# - ...
+# ENV_PACKAGE_SOURCES
+# - 'hello.bash'
+# - ...
+# AUTO_PACKAGE_SUMS
+# - SKIP
+# - ...
+# ENV_PACKAGE_PGPS
+# - 63ADA633FE7468630D9BC56175530B8B9F74CF3A # PGP: Israel Roldan (airvzxf) <israel.alberto.rv@gmail.com>, https://github.com/airvzxf.gpg
+# - ...
+# AUTO_SOURCE_DOWNLOADED
+# - ENV_GITHUB_REPOSITORY-ENV_PACKAGE_VERSION
+# ENV_PACKAGE_INFORMATION
+# - # Maintainer: Israel Roldan <israel.alberto.rv@gmail.com>
+# - # ...
 
 #printenv
 
-# Custom variables
+# Custom variables.
 build_package=01-build-package
-#package_name='brightness-xrandr'
-#version='v1.0.0'
-#git_owner='airvzxf'
-#git_repository_name="archlinux-${package_name}"
 
-# Custom variables: Auto generated
+# Custom variables: auto-generated.
 #github_repository_url="https://github.com/${ENV_GITHUB_OWNER}/${ENV_GITHUB_REPOSITORY}"
-url_pkgbuild_file="https://raw.githubusercontent.com/${ENV_GITHUB_OWNER}/${ENV_GITHUB_REPOSITORY}/${ENV_GITHUB_TAG_VERSION}/archlinux-aur/PKGBUILD"
+url_pkgbuild_file="https://raw.githubusercontent.com/${AUTO_GITHUB_REPOSITORY_UID}/${AUTO_GITHUB_TAG_VERSION}/archlinux-aur/PKGBUILD"
 #aur_project="${package_name}-git"
-
-is_production="false"
-if [[ -n ${ENV_IS_PRODUCTION} ]]; then
-  is_production="${ENV_IS_PRODUCTION}"
-fi
 
 # Path variables
 #ssh_path="/root/.ssh"
@@ -62,14 +120,38 @@ set -xve
 # Download the PKGBUILD and build it.
 mkdir --parents "${build_package}"
 cd "${build_package}"
-curl --output PKGBUILD "${url_pkgbuild_file}"
-makepkg --log --force
+#curl --output PKGBUILD "${url_pkgbuild_file}"
+
+sed --in-place 's|ENV_PACKAGE_INFORMATION|'"${ENV_PACKAGE_INFORMATION}"'|g' PKGBUILD
+sed --in-place 's|ENV_PACKAGE_NAME|'"${ENV_PACKAGE_NAME}"'|g' PKGBUILD
+sed --in-place 's|ENV_PACKAGE_VERSION|'"${ENV_PACKAGE_VERSION}"'|g' PKGBUILD
+sed --in-place 's|ENV_PACKAGE_RELEASE|'"${ENV_PACKAGE_RELEASE}"'|g' PKGBUILD
+sed --in-place 's|ENV_PACKAGE_DESCRIPTION|'"${ENV_PACKAGE_DESCRIPTION}"'|g' PKGBUILD
+sed --in-place 's|ENV_PACKAGE_ARCHITECTURES|'"${ENV_PACKAGE_ARCHITECTURES}"'|g' PKGBUILD
+sed --in-place 's|ENV_PACKAGE_LICENSES|'"${ENV_PACKAGE_LICENSES}"'|g' PKGBUILD
+sed --in-place 's|ENV_PACKAGE_DEPENDENCIES|'"${ENV_PACKAGE_DEPENDENCIES}"'|g' PKGBUILD
+sed --in-place 's|ENV_PACKAGE_PGPS|'"${ENV_PACKAGE_PGPS}"'|g' PKGBUILD
+sed --in-place 's|AUTO_GITHUB_URL|'"${AUTO_GITHUB_URL}"'|g' PKGBUILD
+sed --in-place 's|AUTO_PACKAGE_SOURCES|'"${AUTO_PACKAGE_SOURCES}"'|g' PKGBUILD
+sed --in-place 's|AUTO_SOURCE_DOWNLOADED|'"${AUTO_SOURCE_DOWNLOADED}"'|g' PKGBUILD
+
+AUTO_PACKAGE_SUMS="$(makepkg --geninteg 2> /dev/null)"
+AUTO_PACKAGE_SUMS="${AUTO_PACKAGE_SUMS// /}"
+AUTO_PACKAGE_SUMS="${AUTO_PACKAGE_SUMS//$'\n'/ }"
+sed --in-place 's|md5sums=(AUTO_PACKAGE_SUMS)|'"${AUTO_PACKAGE_SUMS}"'|g' PKGBUILD
+
+namcap -i PKGBUILD
+namcap -i PKGBUILD | grep --quiet '[WE]:' && {
+  echo "ERROR: The package builder file (PKGBUILD) needs improvements."
+  exit 1
+}
+
+makepkg --log --check
 makepkg --printsrcinfo > .SRCINFO
 ls -lha .
 
+cat PKGBUILD
 exit 0
-# TODO: Generate the MD5 sum for tar file.
-#makepkg --geninteg
 
 # Create the AUR SSH keys.
 rm -f "${ssh_config}"
@@ -125,7 +207,7 @@ git config user.name "Israel Roldan"
 git add .
 git commit -m "Automatic deployment on '$(date)' from the official repository in GitHub using CI."
 commit_hash=$(git rev-parse HEAD)
-if [[ ${is_production} == true ]]; then
+if [[ -n ${ENV_IS_PRODUCTION} ]]; then
   git push
 fi
 
--- .github/actions/aur-deployment/test-docker.bash
+++ .github/actions/aur-deployment/test-docker.bash
@@ -16,11 +16,22 @@ docker build --tag "${TAG}" --progress "${DOCKER_PROGRESS}" .
 # Add this option to replace the entrypoint with a bash terminal.
 #  --entrypoint /bin/bash \
 docker run --tty --interactive --rm \
-  --env ENV_PACKAGE_NAME='brightness-xrandr' \
   --env ENV_GITHUB_OWNER='airvzxf' \
   --env ENV_GITHUB_REPOSITORY='archlinux-brightness-xrandr' \
-  --env ENV_GITHUB_TAG_VERSION='v1.0.0' \
-  --env ENV_SSH_PUBLIC_KEY='foo' \
+  --env ENV_GITHUB_TAG_VERSION_PREFIX='v' \
+  --env ENV_GITHUB_TAG_VERSION_SUFFIX='' \
+  --env ENV_PACKAGE_ARCHITECTURES='x86_64' \
+  --env ENV_PACKAGE_DEPENDENCIES='bash xorg-xrandr grep coreutils sed bc' \
+  --env ENV_PACKAGE_DESCRIPTION='Changes the brightness using the command xrandr. Furthermore, adding multiple options.' \
+  --env ENV_PACKAGE_INFORMATION='# Maintainer: Israel Roldan <israel.alberto.rv@gmail.com>' \
+  --env ENV_PACKAGE_LICENSES='GPL3' \
+  --env ENV_PACKAGE_NAME='brightness-xrandr' \
+  --env ENV_PACKAGE_PGPS='63ADA633FE7468630D9BC56175530B8B9F74CF3A' \
+  --env ENV_PACKAGE_RELEASE='1' \
+  --env ENV_PACKAGE_SOURCES='' \
+  --env ENV_PACKAGE_SOURCE_GITHUB='true' \
+  --env ENV_PACKAGE_VERSION='1.0.0' \
   --env ENV_SSH_PRIVATE_KEY='bar' \
+  --env ENV_SSH_PUBLIC_KEY='foo' \
   --name "${NAME}-$(date +%Y%m%d-%H%M%S-%N)" \
   "${TAG}"
